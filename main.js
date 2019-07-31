b6dd310e373f4ad3fe39fa858342ec54be4baf33
Son NK <son@imac-de-son.home>
2019-07-31 08:36:06 +0200

    handle user already login in FB, fix SL

--- main.js
+++ main.js
@@ -1,28 +1,34 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-        fb_user: {
-            email: "",
-            name: "",
-            avatar_url: ""
-        },
-        fb_login: false
-    }
-})
+/*** Init VUE ***/
+const app = new Vue({
+  el: '#app',
+  data: {
+    fb_user: {
+      email: "",
+      name: "",
+      avatar_url: ""
+    },
+    fb_login: false,
+
+    sl_user: {
+      email: "",
+      name: "",
+      avatar_url: ""
+    },
+    sl_login: false
+  }
+});
 
 /*** SimpleLogin, using OIDC library ***/
-
-// SimpleLogin as identity provider
-var providerInfo = OIDC.discover('https://app.simplelogin.io');
+const providerInfo = OIDC.discover('https://app.simplelogin.io');
 
 OIDC.setProviderInfo(providerInfo);
-OIDC.storeInfo(providerInfo, clientInfo);
 
 // OAuth2 client info. Please replace "client-id" here by your SimpleLogin OAuth2 client-id
-var clientInfo = {
-    client_id: 'client-id',
-    redirect_uri: 'http://localhost:8000'
+const clientInfo = {
+  client_id: 'client-id',
+  redirect_uri: 'https://12c7e730.ngrok.io' // todo
 };
+OIDC.storeInfo(providerInfo, clientInfo);
 
 OIDC.setClientInfo(clientInfo);
 
@@ -30,39 +36,54 @@ OIDC.setClientInfo(clientInfo);
 OIDC.restoreInfo();
 
 // Get Access Token
-var token = OIDC.getAccessToken();
+let token = OIDC.getAccessToken();
 
 // Make userinfo request using access_token.
 if (token !== null) {
-    $.get('https://app.simplelogin.io/oauth2/userinfo/?access_token=' + token, function (data) {
-        alert('USERINFO: ' + JSON.stringify(data));
-        console.log(JSON.stringify(data));
-    });
+  fetch('https://app.simplelogin.io/oauth2/userinfo/?access_token=' + token)
+    .then(response => response.json())
+    .then(res => {
+      app.sl_login = true;
+      app.sl_user = {
+        email: res.email,
+        name: res.name,
+        avatar_url: res.avatar_url
+      };
+    })
 }
 
 // Make an authorization request if the user click the login button.
 function simpleLogin() {
-    OIDC.login({
-        scope: 'openid profile email',
-        response_type: 'id_token token'
-    });
+  OIDC.login({
+    scope: 'openid profile email',
+    response_type: 'id_token token'
+  });
 }
+
 /*** END SimpleLogin ***/
+
+function getFBUserData() {
+  FB.api('/me?fields=id,name,email,picture{url}', function (response) {
+    // response has this form {"id":"1234","name":"First Last","email":"abcd@gmail.com","picture":{"data":{"url":"https://avatar.png"}}}
+    app.fb_login = true;
+    app.fb_user = {
+      email: response.email,
+      name: response.name,
+      avatar_url: response.picture.data.url
+    }
+  });
+}
 
 /*** Facebook ***/
 function checkLoginState() {
-    debugger;
-    FB.getLoginStatus(function (response) {
-        FB.api('/me?fields=id,name,email,picture{url}', function (response) {
-            // response has this form {"id":"1234","name":"First Last","email":"abcd@gmail.com","picture":{"data":{"url":"https://avatar.png"}}}
-            app.fb_login = true;
-            app.fb_user = {
-                email: response.email,
-                name: response.name,
-                avatar_url: response.picture.data.url
-            }
-        });
-    });
+  FB.getLoginStatus(function () {
+    getFBUserData();
+  });
 }
+
+FB.getLoginStatus(function (response) {
+  if (response.status === 'connected')
+    getFBUserData();
+});
 
 /*** END Facebook ***/
